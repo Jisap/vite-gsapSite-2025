e802b8f732962e5c0369a65183f7925d92dccd38
Jorge Matos <jisapjisap@gmail.com>
2025-08-15 18:17:51 +0200

    Add MessageSection component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,29 @@ import HeroSection from './sections/HeroSEction'
 import gsap from 'gsap';
 import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import { useGSAP } from '@gsap/react';
+import MessageSection from './sections/MessageSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
 
-  useGSAP(() => {
+  useGSAP(() => { // No es necesario especificar el scope, ya que el hook se ejecuta en el contexto del componente
     ScrollSmoother.create({
-      smooth: 3,
-      effects: true,
+      smooth: 3,                  // Define en segundos cuánto tardará el contenido en alcanzar la posición del scroll 
+      effects: true,              // Le dice a GSAP que busque atributos en el html para crear efectos de parallax
     });
   })
 
   return (
     <div>
       <Navbar />
-      <HeroSection />
-      <div className='h-dvh border border-red-500' />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <div className='h-dvh border border-red-500' />
+        </div>
+      </div>
     </div>
   )
 }
