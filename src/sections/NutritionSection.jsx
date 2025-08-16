a3fa8cfb10ac7548b53b7f7abc4fe6f15f73d03a
Jorge Matos <jisapjisap@gmail.com>
2025-08-16 14:38:46 +0200

    Add GSAP animations to NutritionSection for enhanced text reveal and scroll effects

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -21,6 +21,54 @@ const NutritionSection = () => {
     }
   }, [isMobile]);
 
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".nutrition-title", {                // Dividimos los textos en caracteres individuales
+      type: "chars",
+    });
+    const paragraphSplit = SplitText.create(".nutrition-section p", {       // Dividimos los textos en palabras individuales. Se aplican a todos los p de la sección
+      type: "words, lines",
+      linesClass: "paragraph-line",
+    });
+
+    const contentTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".nutrition-section",                      // La animación comienza cuando la parte superior de la sección (.nutrition-section) llega al centro de la pantalla (start: "top center").
+        start: "top center",
+        toggleActions: "play reverse play reverse",
+      },
+    });
+    contentTl
+      .from(titleSplit.chars, {                             // Animación del nutrition-title "It still does" y a los p de la sección
+        yPercent: 100,                                      // Cada letra entra deslizándose desde abajo (100% de su propia altura).
+        stagger: 0.02,                                      // Se aplica un pequeño retraso de 0.02 segundos entre la animación de cada letra, 
+        ease: "power2.out",
+        
+      })
+      .from(paragraphSplit.words, {                         // Animacion del parrafo 
+        yPercent: 300,                                      // Cada palabra entra desde mucho más abajo (300% de su altura). 
+        rotate: 3,                                          // Entran con una ligera rotación de 3 grados.
+        ease: "power1.inOut", 
+        duration: 1,
+        stagger: 0.01,                                      // Con un retraso aún más rápido entre cada palabra para un efecto fluido
+      });
+
+    const titleTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".nutrition-section",
+        start: "top 80%",
+        toggleActions: "play reverse play reverse",
+      },
+    });
+
+    titleTl.to(".nutrition-text-scroll", {                  //Animación del título "Body Good"
+      duration: 1,
+      opacity: 1,                                           // Se le aplica una opacidad de 1 para revelar el text  
+      clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",  // Se aplica un clipPath que tiene forma de un rectángulo de 100% de altura y ancho
+      ease: "power1.inOut",
+    });
+
+  });
+
   return (
     <section className="nutrition-section">
       <img
